360eafab18e88aa1045cb7a6aaaab065e5af4bbb
Lucas Brasil Silva <lucasbrasil396@gmail.com>
2023-10-10 15:04:27 -0300

    Update Code

--- guitar_flash.py
+++ guitar_flash.py
@@ -11,30 +11,34 @@ def encontrar_e_click_simples(imagem):
     coord = pyautogui.locateCenterOnScreen(imagem)
     pyautogui.click(coord[0], coord[1], clicks=1, duration=1, button='left')
 
-# Abre uma nova guia no site do jogo:
-webbrowser.open('https://guitarflash.com/')
-sleep(6)
-# clicar no botão jogar:
-pyautogui.click(2097, 392,duration=1)
-sleep(5)
-# Clicar no nivel e na musica:
-encontrar_e_click_simples('nivel.PNG')
-pyautogui.click(2093, 879, duration=2)
-sleep(5)
-# Rolar a para alinhar com as coordenadas:
-pyautogui.scroll(-200)
-sleep(10)
-# Clicar na tecla space para começar a jogar:
-pyautogui.press('space')
-sleep(4.5)
-# Reconhece se o disco está na coordenada necessária para pressionar a respectiva tecla:
-while keyboard.is_pressed('space') == False:
-    if pyautogui.pixelMatchesColor(2134, 823,(0, 152, 0)):
-        pyautogui.press('a')
-        sleep(0.095)
-    if pyautogui.pixelMatchesColor(2225, 824,(255, 0, 0)):
-        pyautogui.press('s')
-        sleep(0.095)
-    if pyautogui.pixelMatchesColor(2300, 820, (244, 244, 2)):
-        pyautogui.press('j')
-        sleep(0.095)
+def main():
+    # Abre uma nova guia no site do jogo:
+    webbrowser.open('https://guitarflash.com/')
+    sleep(6)
+    # clicar no botão jogar:
+    pyautogui.click(2097, 392,duration=1)
+    sleep(5)
+    # Clicar no nivel e na musica:
+    encontrar_e_click_simples('nivel.PNG')
+    pyautogui.click(2093, 879, duration=2)
+    sleep(5)
+    # Rolar a para alinhar com as coordenadas:
+    pyautogui.scroll(-200)
+    sleep(10)
+    # Clicar na tecla space para começar a jogar:
+    pyautogui.press('space')
+    sleep(4.5)
+    # Reconhece se o disco está na coordenada necessária para pressionar a respectiva tecla:
+    while keyboard.is_pressed('space') == False:
+        if pyautogui.pixelMatchesColor(2134, 823,(0, 152, 0)):
+            pyautogui.press('a')
+            sleep(0.095)
+        if pyautogui.pixelMatchesColor(2225, 824,(255, 0, 0)):
+            pyautogui.press('s')
+            sleep(0.095)
+        if pyautogui.pixelMatchesColor(2313, 833, (244, 244, 2)):
+            pyautogui.press('j')
+            sleep(0.095)
+
+if __name__ == '__main__':
+    main()
